7c8b94003e09f7eefcb29c894da24224356e29d3
Muhammadwali10 <mohamedmowlid224@yahoo.com>
2024-06-30 18:34:19 +0300

    add challenge1.js,challenge2.js,challenge3.js

---
+++ challenge1.js
@@ -0,0 +1,16 @@
+function getStudentGrade() {
+    let marks = x;
+    if (marks > 79) {
+        console.log("A");
+    } else if (marks >= 60) {
+        console.log("B");
+    } else if (marks >= 49) {
+        console.log("C");
+    } else if (marks >= 40) {
+        console.log("D");
+    } else {
+        console.log("E");
+    }
+
+}
+getStudentGrade();
